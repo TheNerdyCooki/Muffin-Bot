7649c2b9a93e38bd971934249299006141367b47
thenerdycooki <zeekzz.js@gmail.com>
2021-03-23 19:45:07 -0500

    E

--- src/events/ready/ReadyEvent.js
+++ src/events/ready/ReadyEvent.js
@@ -15,15 +15,7 @@ module.exports = class ReadyEvent extends BaseEvent {
 			`Loaded ${this.client.events.size} events!`
 		].join('\n'));
 
-		const activities = [
-			`${this.client.guilds.cache.size} servers!`,
-			`${this.client.channels.cache.size} channels!`,
-			`${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} users!`
-		];
-
-		let i = 0;
-		setInterval(() => this.client.user.setActivity(`${this.client.prefix}help | ${activities[i++ % activities.length]}`, { type: 'WATCHING' }), 15000);
-
+		
   }
 
 
