4a0406e65d3128e761a653e5f8909e1578110df7
thenerdycooki <zeekzz.js@gmail.com>
2021-06-15 11:53:03 -0500

    ugh

--- src/events/ready/ReadyEvent.js
+++ src/events/ready/ReadyEvent.js
@@ -7,23 +7,9 @@ module.exports = class ReadyEvent extends BaseEvent {
     super('ready');
   }
   async run (client) {
+    console.log(client.user.tag + ' has logged in.');
 
-
-    console.log([
-			client.user.tag + ' has logged in.',
-			client.user.tag + 'has loaded commands',
-			client.user.tag + 'has loaded events'
-		].join('\n'));
-
-		const activities = [
-			`${this.client.guilds.cache.size} servers!`,
-			`${this.client.channels.cache.size} channels!`,
-			`${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} users!`
-		];
-
-		let i = 0;
-		setInterval(() => this.client.user.setActivity(`${this.client.prefix}help | ${activities[i++ % activities.length]}`, { type: 'WATCHING' }), 15000);
-
+    
 
 		
   }
